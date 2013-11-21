b9eec8c40160625a7159693b8bec4e286369447e
Andres Berardoni <andres.berardoni@engee.com.ar>
2013-11-21 16:24:26 -0300

    transparent buttons

--- startfighter/gen/com/startfighter/R.java
+++ startfighter/gen/com/startfighter/R.java
@@ -22,16 +22,26 @@ public final class R {
     }
     public static final class drawable {
         public static final int exit_a=0x7f020000;
-        public static final int exit_b=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int splash=0x7f020003;
-        public static final int start_a=0x7f020004;
-        public static final int start_b=0x7f020005;
+        public static final int exit_at=0x7f020001;
+        public static final int exit_b=0x7f020002;
+        public static final int exit_bt=0x7f020003;
+        public static final int exitselector=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int splash=0x7f020006;
+        public static final int start_a=0x7f020007;
+        public static final int start_at=0x7f020008;
+        public static final int start_b=0x7f020009;
+        public static final int start_bt=0x7f02000a;
+        public static final int startselector=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int creditsText=0x7f080001;
-        public static final int splashScreenImage=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int btnExit=0x7f080003;
+        public static final int btnStart=0x7f080002;
+        public static final int buttons=0x7f080001;
+        public static final int creditsText=0x7f080005;
+        public static final int mainMenuImage=0x7f080000;
+        public static final int splashScreenImage=0x7f080004;
     }
     public static final class layout {
         public static final int activity_start_fighter=0x7f030000;
